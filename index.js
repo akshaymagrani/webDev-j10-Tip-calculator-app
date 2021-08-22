2eded253bc12ff0ed550556fb2a4529cb6ae55d0
akshaymagrani <akshaymagrani1@gmail.com>
2021-08-22 18:36:51 +0530

    resolving no people exception

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ let totalAmount = document.getElementById("result-total");
 let people = document.getElementsByClassName("no-of-people");
 
 function tip(btn){
-    var a = (parseInt(btn.innerText.slice(0,2)) * bill[0].value /(100*people[0].value)).toFixed(2);
-    console.log(tipAmount.innerText);
-    tipAmount.innerText = tipAmount.innerText.slice(0,1) + ' ' + a;
-    function total(){
-        let total = (parseInt(bill[0].value)/people[0].value + parseInt(a)).toFixed(2);  
-        console.log(typeof a, typeof bill[0].value);
-        totalAmount.innerText = totalAmount.innerText.slice(0,1) + ' ' + total;
+    if(people[0].value == ''){
+        people[0].value = 1;
     }
+        var a = (parseInt(btn.innerText.slice(0,2)) * bill[0].value /(100*people[0].value)).toFixed(2);
+        console.log(tipAmount.innerText);
+        tipAmount.innerText = tipAmount.innerText.slice(0,1) + ' ' + a;
+        function total(){
+            let total = (parseInt(bill[0].value)/people[0].value + parseInt(a)).toFixed(2);  
+            console.log(typeof a, typeof bill[0].value);
+            totalAmount.innerText = totalAmount.innerText.slice(0,1) + ' ' + total;
+        }
+  
     total();
     return a;
 }
